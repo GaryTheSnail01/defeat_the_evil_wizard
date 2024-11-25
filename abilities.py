31fcafe95f718bb94b8072fd3089df6d25d95ff1
Garrett Cross <garrettcrosskc@gmail.com>
2024-11-25 16:37:36 -0600

    Added the functionality of each special ability. Final touches.

--- abilities.py
+++ abilities.py
@@ -3,42 +3,54 @@ import random
 def shield_block(self):
     if self.shield == False:
         self.shield = True
-        print('You raise your shield high, bracing for an attack!')
+        print(f'{self.name} raises their shield high, bracing for an attack!')
     else:
-        print("Your shield is already raised!")
-        return
+        print(f"{self.name}'s shield is already raised!")
         
 def focus_magical_energy(self):
     self.attack_modifier += .5
-    print("You feel the might of your magical energy building within you...")
+    print(f"{self.name} feels the might of magical energy building within them, increasing future damage...")
     
-def wild_shape(self):
+def wild_shape(self, opponent):
     if self.wild_shape == True:
         self.wild_shape = False
         self.attackmodifer = 1
-        print("You've shifted back into your human form...")
+        print(f"{self.name} shifted back into their human form...")
     else:
         self.wild_shape = True
-        self.attackmodifer = 1.5
-        print("Your form shifts and forms to become a massive bear! Lets see how the wizard will handle this...")
+        self.attack_modifer = 1.5
+        print(f"{self.name}'s form shifts into a massive bear! Lets see how {opponent.name} will handle this...")
         
-def cast_sleep(opponent):
+def cast_sleep(self, opponent):
     if opponent.sleep <= 0:
         opponent.sleep += random.randint(2, 3)
-        print("You pluck the strings of your lute and sing a soothing song to the wizard. He's now fast asleep...")
+        print(f"{self.name} plucks the strings of their lute and sing a soothing song to {opponent.name}. \nThey're now fast asleep...")
     else:
-        print("The wizard is already asleep, your song doesn't affect your opponent...")
-        return
+        print(f"{opponent.name} is already asleep, {self.name}'s song doesn't affect {opponent.name}...")
     
-def dazzling_flurry(opponent):
-    pass # attack twice in a brilliant dance of blows
+def dazzling_flurry(self, opponent):
+    max_attack = int((self.attack_power * self.attack_modifier) * 2)
+    total_attack = random.randint(int(max_attack / 2), max_attack)
+    opponent.health -= total_attack
+    print(f"{self.name} dances with a brilliant display of blows and attacks dealing {total_attack} damage!")
 
-def battle_cry(self):
-    pass # boost damage by 50% and reduce incoming damage by 25%
+def battle_cry(self, opponent):
+    self.attack_modifier = 1.5
+    opponent.attack_modifier = .75
+    print(f"{self.name} goes into a rage, increasing their damage and reducing any incoming damage!")
 
 def mana_shield(self):
-    pass # reduce incoming damage by 50%
+    if self.shield == False:
+        self.shield = True
+        print(f"A magical barrier forms around {self.name}, reducing incoming damage!")
+    else:
+        print(f"{self.name}'s mana shield is already up!")
 
-def thorn_armor(opponent):
-    pass # deal 25% of damage delt to you from your opponent
+def thorn_armor(self):
+    if self.thorn_armor == False:
+        self.thorn_armor = True
+        print(f"A wild suit of prickely armor forms around {self.name}, dealing damage in retaliation of taking damage.")
+    else:
+        print(f"There's already a suit of thorn armor protecting {self.name}.")
+    
 
--- classes.py
+++ classes.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from abilities import shield_block, focus_magical_energy, wild_shape, cast_sleep
+from abilities import shield_block, focus_magical_energy, wild_shape, cast_sleep, battle_cry, thorn_armor, mana_shield, dazzling_flurry
 import random
 
 @dataclass
@@ -30,58 +30,73 @@ class Character:
             opponent.shield = False
             total_attack = int(total_attack / 2)
             opponent.health -= total_attack
+            print(f"{self.name} attacks {opponent.name} while their defenses are up, \nreducing the incoming attack to {total_attack} damage and stunning themselves in the process!")
+            self.sleep += 1
+        elif opponent.player_class == "Druid" and opponent.thorn_armor == True:
+            return_damage = int(total_attack * .5)
+            print(f"{self.name} attacks {opponent.name} for {total_attack} damage, but takes {return_damage} damage in return!")
+            opponent.health -= total_attack
+            self.health -= return_damage
         else:
             opponent.health -= total_attack
+            print(f"{self.name} attacks {opponent.name} for {total_attack} damage!")
     
-        print(f"{self.name} attacks {opponent.name} for {total_attack} damage!")
         if opponent.health <= 0:
             print(f"{opponent.name} has been defeated!")
                 
 
     def display_stats(self):
-        print(f"{self.name}'s Stats - Health: {self.health}/{self.max_health}, Attack Power: {self.attack_power * self.attack_modifier}")
+        print(f"{self.name}'s Stats - Health: {self.health}/{self.max_health}, Attack Power: {int(self.attack_power * self.attack_modifier)}")
         
     def heal(self):
         add_health = random.randrange(10, 20, 5)
         self.health += add_health
         if self.health > self.max_health:
             self.health = self.max_health
-            print(f"You've healed to max health! Health: {self.health}/{self.max_health}")
+            print(f"{self.name} healed to max health! Health: {self.health}/{self.max_health}")
         else:    
-            print(f"You healed {add_health}HP! Health: {self.health}/{self.max_health}")
+            print(f"{self.name} healed {add_health}HP! Health: {self.health}/{self.max_health}")
             
     def special_abilities(self, opponent):
-        option = 1
+        option = 1 # used for menu options
         abilities = self.class_abilities.get(self.player_class, []) # get the list of special abilities based on the player's class. Returns an empty list in case class is unknown.
         
         for a in abilities:
             print(f"{option} - {a}")
             option += 1
-        
-        try:    
-            selection = int(input("Choose an ability: "))
             
-            if abilities[selection - 1] == "Shield Block":
-                shield_block(self)
-            elif abilities[selection - 1] == "EMPTY":
-                pass
-            elif abilities[selection - 1] == "Focus Magical Energy":
-                focus_magical_energy(self)
-            elif abilities[selection - 1] == "":
-                pass
-            elif abilities[selection - 1] == "Wild Shape":
-                wild_shape(self)
-            elif abilities[selection - 1] == "":
-                pass
-            elif abilities[selection - 1] == "Cast Sleep":
-                cast_sleep(opponent)
-            elif abilities[selection - 1] == "":
-                pass
-            else:
-                pass
-        except ValueError:
-            print("Invalid input. Please enter a number.")
-            return
+        while True:
+            try:    
+                selection = int(input("Choose an ability: "))
+                
+                if abilities[selection - 1] == "Shield Block":
+                    shield_block(self)
+                    break
+                elif abilities[selection - 1] == "Battle Cry":
+                    battle_cry(self, opponent)
+                    break
+                elif abilities[selection - 1] == "Focus Magical Energy":
+                    focus_magical_energy(self)
+                    break
+                elif abilities[selection - 1] == "Mana Shield":
+                    mana_shield(self)
+                    break
+                elif abilities[selection - 1] == "Wild Shape":
+                    wild_shape(self, opponent)
+                    break
+                elif abilities[selection - 1] == "Thorn Armor":
+                    thorn_armor(self)
+                    break
+                elif abilities[selection - 1] == "Cast Sleep":
+                    cast_sleep(self, opponent)
+                    break
+                elif abilities[selection - 1] == "Dazzling Flurry":
+                    dazzling_flurry(self, opponent)
+                    break
+                else:
+                    print("Please enter a valid number.")
+            except ValueError:
+                print("Invalid input. Please enter a number.")
 
 # Warrior class (inherits from Character)
 class Warrior(Character):
@@ -97,19 +112,20 @@ class Mage(Character):
         if self.attack_modifier > 1:
             result = super().attack(opponent) # store the result of the attack
             self.attack_modifier = 1 # reset the modifer to 1
-            print("The power of your focused energy adds to your magical blast!")
+            print(f"The power of {self.name}'s focused energy adds to their magical blast!")
             return result # then return the original result before the modifer was changed
         else:
             return super().attack(opponent)
 
 class Druid(Character):
-    def __init__(self, name, wild_shape = False):
+    def __init__(self, name, wild_shape = False, thorn_armor = False):
         super().__init__(name, health=120, attack_power=30, player_class = "Druid")
         self.wild_shape = wild_shape
+        self.thorn_armor = thorn_armor
             
     def attack(self, opponent):
         if self.wild_shape == True:
-            print("You swipe at the wizard with your massive bear claws!")
+            print(f"{self.name} swipes at {opponent.name} with their massive bear claws!")
             return super().attack(opponent)
         else:
             return super().attack(opponent)
@@ -136,7 +152,7 @@ class EvilWizard(Character):
     def attack(self, opponent):
         if self.sleep > 0:
             self.sleep -= 1
-            print("The wizard is sleeping and cannot attack...")
+            print(f"{self.name} cannot attack...")
             return
         else:
             return super().attack(opponent)
